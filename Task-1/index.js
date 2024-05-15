4738262cebe0fc2824a17a4110fab695ea1bf2c7
prasad <kolaganiprasad01@gmail.com>
2024-05-15 14:25:27 +0530

    added behaviour

---
+++ Task-1/index.js
@@ -0,0 +1,23 @@
+const display = document.getElementById('display')
+
+function appendtodisplay(input){    
+    display.value += input;
+}
+
+function calc(){
+    try{
+        console.log(display.value);
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
+
+function del(){
+    display.value = display.value.slice(0, -1);
+}
+
+function clears(){
+    display.value = "";
+}
